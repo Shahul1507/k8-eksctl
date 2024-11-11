061bfbe9a9483f7bcf5309358d33f7134396b657
Shahul1507 <shahul.met@gmail.com>
2024-11-11 09:47:52 +0530

    k8-eksctl

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "sg_id" {
   type        = string
-  default     = "sg-0fea5e49e962e81c9"
+  default     = "sg-0844fcfa6ab24581f"
   description = "description"
 }
 
 variable "public_subnet_id" {
     type = string
-    default = "subnet-0ea509ad4cba242d7"
+    default = "subnet-015dcad509f833802"
 }
--- workstation.tf
+++ workstation.tf
@@ -1,15 +1,26 @@
+
 module "workstation" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "workstation"
-
   instance_type          = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
   # convert StringList to list and get first element
   subnet_id = var.public_subnet_id
   ami = data.aws_ami.ami_info.id
+    root_block_device = [
+    {
+      volume_size           = 50
+      volume_type           = "gp3"
+      delete_on_termination = true
+    }
+  ]
   user_data = file("workstation.sh")
   tags = {
         Name = "workstation"
     }
 }
+
+
+
+
